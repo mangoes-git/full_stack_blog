2c42a7ced661065aeca8a31e20c01be354f5f125
Mang Yau <mangjieng@gmail.com>
2021-04-25 12:54:25 -0400

    add user schema

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    hash: String,
+    salt: String
+});
+
+userSchema.methods.setPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto
+                      .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                      .toString('hex');
+};
+
+userSchema.methods.isValidPassword = function (password) {
+    const hash = crypto
+                       .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                       .toString('hex');
+    return this.hash === hash;
+};
+
+mongoose.model('User', userSchema, 'Users');
